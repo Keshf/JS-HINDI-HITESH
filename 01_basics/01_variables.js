de86b7f8968ba2b3b0afa042ae2e39de1851d9f2
Keshf <114135814+Keshf@users.noreply.github.com>
2024-01-08 05:45:39 +0000

    packege file and varibles scope commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234455
+let accountBalance = 1123453.56
+let accountEmail = "mohd@gmail.com"
+var accountCity = "Bulandshahr"
+
+
+accountType = "CurrentAccount"
+
+// accountId  = 224455 // const assignment not allowed 
+
+// displaying single line output to the console.
+console.log(accountId)
+
+// displaying tabular output to the console.
+console.table([accountId, accountBalance, accountEmail, accountCity, accountType])
+
+
+
